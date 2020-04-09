b235462a46145533a71fd3c800106188566fc44e
Franco Fadini <franco@made2.co>
2020-04-09 00:01:15 -0300

    Add bottom tabs navigation
    asd

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import {
   StyleSheet,
@@ -12,36 +12,38 @@ import {
 
 function HomeScreen() {
   return (
-    <View style={styles.mainView}>
-      <Text style={styles.mainText}>Home Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const Stack = createStackNavigator();
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  mainView:{
-    flex: 1,
-    justifyContent: 'center',
 
-  },
-  mainText: {
-    textAlign: 'center',
-  }
 });
 
 export default App;
