88226aa136be4198a4fa3aec8773877c458fc80d
Franco Fadini <franco@made2.co>
2020-04-08 22:46:36 -0300

    Add navigation library

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,8 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import {
   StyleSheet,
   View,
@@ -6,13 +10,25 @@ import {
   StatusBar,
 } from 'react-native';
 
+function HomeScreen() {
+  return (
+    <View style={styles.mainView}>
+      <Text style={styles.mainText}>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
     <>
-    <StatusBar barStyle="dark-content" />
-    <View style={styles.mainView}>
-      <Text style={styles.mainText}> RN Base Project</Text>
-    </View>
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
