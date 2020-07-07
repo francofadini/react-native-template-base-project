282c354beb3fed46d4c85ef36f04c9600f15e20c
Franco Fadini <franco@made2.co>
2020-07-07 16:12:15 -0300

    Remove screens folder

--- template/src/Screens/posts/PostsConfigurator.ts
+++
@@ -1,9 +0,0 @@
-import PostsPresenter, { PostsView } from 'screens/posts/PostsPresenter';
-import { AxiosPostsService } from 'services/posts/axios-posts.service';
-
-export default class PostsConfigurator {
-    static buildPresenter = (view: PostsView) => {
-        let postsService = new AxiosPostsService()
-        return new PostsPresenter(view, postsService);
-    }
-}
