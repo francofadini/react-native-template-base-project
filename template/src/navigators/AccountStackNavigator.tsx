4172c7c1aea21aef42aa2d36d101b8e524e39703
Franco Fadini <franco@made2.co>
2020-09-27 20:55:32 -0300

    Fix account navigation UI

--- template/src/navigators/AccountStackNavigator.tsx
+++ template/src/navigators/AccountStackNavigator.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ExampleScreen } from 'screens/ExampleScreen';
+import { DefaultNavigatorScreenOptions } from 'navigators/DefaultNavigatorScreenOptions';
+import strings from 'constants/strings';
 
 const Stack = createStackNavigator();
 
 function AccountStackNavigator() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Account" component={ExampleScreen} />
+    <Stack.Navigator
+      screenOptions={DefaultNavigatorScreenOptions}>
+      <Stack.Screen 
+        name="Account"
+        options={{
+          title: strings.accountScreen.title
+        }}
+        component={ExampleScreen} />
     </Stack.Navigator>
   );
 }
